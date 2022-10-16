150fd05129d1d2252de7c42b5a563d9a6c424b25
Faisal <ansarifaisal480@gmail.com>
2022-10-16 14:23:54 +0530

    Undoing the commented input lines

--- Exercise_11/Exercise_11.py
+++ Exercise_11/Exercise_11.py
@@ -7,7 +7,7 @@ from types import SimpleNamespace
 speak = Dispatch("SAPI.SpVoice")
 # speak.Speak("Hello World")
 
-r = requests.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=5621f945a9c746baa86da2f1b394d166")
+r = requests.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=5621f945a9c746baa86da2f1b394d1661")
 data = r.json()
 
 description = data['articles'][0]['description']
